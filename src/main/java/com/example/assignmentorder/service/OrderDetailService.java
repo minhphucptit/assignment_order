7c9017d8b482c6f0159c82958d5d9b7a0de8d73f
minhphucptit <nminhphuc99@gmail.com>
2024-04-19 23:00:56 +0700

    delete redundant features

--- src/main/java/com/example/assignmentorder/service/OrderDetailService.java
+++
@@ -1,7 +0,0 @@
-package com.example.assignmentorder.service;
-
-import com.example.assignmentorder.entity.OrderDetail;
-import com.example.assignmentorder.service.base.BaseService;
-
-public interface OrderDetailService extends BaseService<OrderDetail, Integer> {
-}
